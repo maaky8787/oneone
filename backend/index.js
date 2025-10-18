5f6d7ad8a3ff7f7ded4fbe552d05dd0b0b3241d2
mekkihassanelawad <basherbasher1p@gmail.com>
2025-10-18 22:22:19 +0300

    pث

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ import rateLimit from 'express-rate-limit';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
