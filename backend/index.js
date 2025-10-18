c5c5fc19a9e2fd9b6527407a8c38d181a4b2c0c0
mekkihassanelawad <basherbasher1p@gmail.com>
2025-10-18 23:47:25 +0300

    oop

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ import rateLimit from 'express-rate-limit';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
--- backend/supabase.js
+++ backend/supabase.js
@@ -1,12 +1,7 @@
 import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-import { createClient } from '@supabase/supabase-js'
+dotenv.config();
 
-// Ensure we load the project's root .env file even when this module is imported
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-dotenv.config({ path: resolve(__dirname, '../.env') });
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
